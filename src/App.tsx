1277ef94aaa002249424872bf6906e8b805e4b68
Aryan Yadav <aaryany2505@gmail.com>
2026-06-10 13:51:14 +0530

    Clean up the CodePage Code

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,12 @@
 import { useEffect, useState } from 'react'
 
 import './App.css'
-import CodeEditor from './components/CodeEditor'
 import Button from './components/Button'
-import useDebounce from './Hooks/useDebounce'
-import {saveCode,loadCode} from './utils/db.ts'
 import {supabase} from "./utils/supabase.ts"
+import CodePage from './pages/CodePage.tsx'
 function App() {
 
-const [html,setHtml] = useState('')
-const [css,setCss] = useState('')
-const [js,setJs] = useState('')
-
-const debouceHTML = useDebounce(html, 1000)
-const debouceCSS = useDebounce(css, 1000)
-const debouceJS = useDebounce(js, 1000)
-const [user,setuser] = useState(null)
-
+ const [user,setuser] = useState(null)
 useEffect(() => {
   const getUser = async()=>{
     const {data} = await supabase.auth.getUser();
@@ -26,20 +16,9 @@ useEffect(() => {
 
   }
   getUser()
-  loadCode().then((data) => {
-     if (data) {
-      setHtml(data.html || "");
-      setCss(data.css || "");
-      setJs(data.js || "");
-    }
-    console.log("data loaded");
-    
-  });
+ 
 }, []);
-const SaveToDb=()=>{
-  saveCode(debouceHTML, debouceCSS, debouceJS);
-  console.log(user)
-}
+
 const SignInWithGoogle=async()=>{
   const {data,error} = await supabase.auth.signInWithOAuth({
   provider: 'google',
@@ -55,40 +34,18 @@ const SignInWithGoogle=async()=>{
   return (
     <>
     {user ? (
+      <>
       <Button name="Sign Out" onClick={() => supabase.auth.signOut()} />
+       <CodePage  />
+      </>
     ) : (
-      <Button name="Sign In With Google" onClick={SignInWithGoogle} />
+      <div className="w-full h-screen flex items-center justify-center">
+        <Button name="Sign In With Google" onClick={SignInWithGoogle} />
+
+      </div>
     )}
-    <div className="  w-full h-screen bg-gray-800 overflow-hidden">
-    <div className ="w-full ">
-      <Button name="Save" onClick={SaveToDb}  />
- {/* <div className="flex gap-2 p-2">
-
-   <Button name="HTML" onClick={() => setActiveTab("html")} />
-   <Button name="CSS" onClick={() => setActiveTab("css")} />
-   <Button name="JavaScript" onClick={() => setActiveTab("js")} />
-    </div> */}
-  <div className="w-full flex gap-2 p-2">
-
-  {/* {activeTab === "html" && ( */}
-          <CodeEditor language="html" value={html} onChange={setHtml} />
-        {/* // )} */}
-        {/* {activeTab ==="css"&&( */}
-          <CodeEditor language="css" value={css} onChange={setCss} />
-        {/* )} */}
-        {/* {activeTab ==="js"&&( */}
-          <CodeEditor language="javascript" value={js} onChange={setJs} />
-        {/* )} */}
-  </div>
-    </div>
-   <div className="w-full h-2/3">
-    <iframe
-          srcDoc={`<html><style>${debouceCSS}</style><body>${debouceHTML}<script>${debouceJS}</script></body></html>`}
-          className="w-full h-full bg-white"
-        />
-   </div>
-      
-    </div>
+   
+ 
     </>
    
   )
---
+++ src/pages/CodePage.tsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import Button from '../components/Button'
+import { useEffect, useState } from 'react'
+import CodeEditor from '../components/CodeEditor'
+import useDebounce from '../Hooks/useDebounce'
+import {saveCode,loadCode} from '../utils/db.ts'
+
+function CodePage() {
+    const [html,setHtml] = useState('')
+    const [css,setCss] = useState('')
+    const [js,setJs] = useState('')
+    
+    const debouceHTML = useDebounce(html, 1000)
+    const debouceCSS = useDebounce(css, 1000)
+    const debouceJS = useDebounce(js, 1000)
+
+   const SaveToDb=()=>{
+     saveCode(debouceHTML, debouceCSS, debouceJS);
+    
+   }
+    useEffect(()=>{
+         loadCode().then((data) => {
+             if (data) {
+              setHtml(data.html || "");
+              setCss(data.css || "");
+              setJs(data.js || "");
+            }
+            console.log("data loaded");
+            
+          });
+    })
+  return (
+   <div className="  w-full h-screen bg-gray-800 overflow-hidden">
+    <div className ="w-full ">
+      <Button name="Save" onClick={SaveToDb}  />
+ 
+  <div className="w-full flex gap-2 p-2">
+
+          <CodeEditor language="html" value={html} onChange={setHtml} />
+          <CodeEditor language="css" value={css} onChange={setCss} />
+          <CodeEditor language="javascript" value={js} onChange={setJs} />
+  </div>
+    </div>
+   <div className="w-full h-2/3">
+    <iframe
+          srcDoc={`<html><style>${debouceCSS}</style><body>${debouceHTML}<script>${debouceJS}</script></body></html>`}
+          className="w-full h-full bg-white"
+        />
+   </div>
+      
+    </div>
+  )
+}
+
+export default CodePage
