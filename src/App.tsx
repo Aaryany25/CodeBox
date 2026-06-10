54eb159236807b607b7f35c5bebf962e66b42623
Aryan Yadav <aaryany2505@gmail.com>
2026-06-10 13:20:20 +0530

    Added supabase Authentication

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import CodeEditor from './components/CodeEditor'
 import Button from './components/Button'
 import useDebounce from './Hooks/useDebounce'
 import {saveCode,loadCode} from './utils/db.ts'
+import {supabase} from "./utils/supabase.ts"
 function App() {
 
 const [html,setHtml] = useState('')
@@ -14,7 +15,17 @@ const [js,setJs] = useState('')
 const debouceHTML = useDebounce(html, 1000)
 const debouceCSS = useDebounce(css, 1000)
 const debouceJS = useDebounce(js, 1000)
+const [user,setuser] = useState(null)
+
 useEffect(() => {
+  const getUser = async()=>{
+    const {data} = await supabase.auth.getUser();
+    if(data){
+    setuser(data.user)
+    }
+
+  }
+  getUser()
   loadCode().then((data) => {
      if (data) {
       setHtml(data.html || "");
@@ -22,14 +33,32 @@ useEffect(() => {
       setJs(data.js || "");
     }
     console.log("data loaded");
-    // console.log(data);
+    
   });
 }, []);
 const SaveToDb=()=>{
   saveCode(debouceHTML, debouceCSS, debouceJS);
+  console.log(user)
+}
+const SignInWithGoogle=async()=>{
+  const {data,error} = await supabase.auth.signInWithOAuth({
+  provider: 'google',
+  })
+ if(data){
+  setuser(data.user);
+ }
+
+   if (error) {
+    console.log(error);
+  }
 }
   return (
-    
+    <>
+    {user ? (
+      <Button name="Sign Out" onClick={() => supabase.auth.signOut()} />
+    ) : (
+      <Button name="Sign In With Google" onClick={SignInWithGoogle} />
+    )}
     <div className="  w-full h-screen bg-gray-800 overflow-hidden">
     <div className ="w-full ">
       <Button name="Save" onClick={SaveToDb}  />
@@ -60,6 +89,7 @@ const SaveToDb=()=>{
    </div>
       
     </div>
+    </>
    
   )
 }
